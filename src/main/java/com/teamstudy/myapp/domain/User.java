e773e8da5b2d5808bb03b6186f24c47a3d8ce226
cristianrhymes <criferriv@alum.us.es>
2015-04-07 11:39:35 +0200

    Añadida la imagen de usuario
    * Se ha añadido el atributo imageUrl para almacenar la url de la imagen
    de usuario y, posteriormente, mostrarla en el frontend

--- src/main/java/com/teamstudy/myapp/domain/User.java
+++ src/main/java/com/teamstudy/myapp/domain/User.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -21,163 +22,177 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Document(collection = "T_USER")
 public class User extends AbstractAuditingEntity implements Serializable {
 
-    @Id
-    private String id;
+	@Id
+	private String id;
 
-    @NotNull
-    @Pattern(regexp = "^[a-z0-9]*$")
-    @Size(min = 1, max = 50)
-    private String login;
+	@NotNull
+	@Pattern(regexp = "^[a-z0-9]*$")
+	@Size(min = 1, max = 50)
+	private String login;
 
-    @JsonIgnore
-    @NotNull
-    @Size(min = 5, max = 100)
-    private String password;
+	@JsonIgnore
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String password;
 
-    @Size(max = 50)
-    @Field("first_name")
-    private String firstName;
+	@Size(max = 50)
+	@Field("first_name")
+	private String firstName;
 
-    @Size(max = 50)
-    @Field("last_name")
-    private String lastName;
+	@Size(max = 50)
+	@Field("last_name")
+	private String lastName;
 
-    @Email
-    @Size(max = 100)
-    private String email;
+	@Email
+	@Size(max = 100)
+	private String email;
 
-    private boolean activated = false;
+	private boolean activated = false;
 
-    @Field("is_teacher")
-    private boolean isTeacher;
+	@Field("is_teacher")
+	private boolean isTeacher;
 
-    @Size(min = 2, max = 5)
-    @Field("lang_key")
-    private String langKey;
+	@Size(min = 2, max = 5)
+	@Field("lang_key")
+	private String langKey;
 
-    @Size(max = 20)
-    @Field("activation_key")
-    private String activationKey;
+	@Size(max = 20)
+	@Field("activation_key")
+	private String activationKey;
 
-    @JsonIgnore
-    private Set<Authority> authorities = new HashSet<>();
+	@URL
+	private String imageUrl;
 
-    public String getId() {
-        return id;
-    }
+	@JsonIgnore
+	private Set<Authority> authorities = new HashSet<>();
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public boolean getActivated() {
-        return activated;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setActivated(boolean activated) {
-        this.activated = activated;
-    }
+	public boolean getActivated() {
+		return activated;
+	}
 
-    public boolean isTeacher() {
-        return isTeacher;
-    }
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
 
-    public void setTeacher(boolean isTeacher) {
-        this.isTeacher = isTeacher;
-    }
+	public boolean isTeacher() {
+		return isTeacher;
+	}
 
-    public String getActivationKey() {
-        return activationKey;
-    }
+	public void setTeacher(boolean isTeacher) {
+		this.isTeacher = isTeacher;
+	}
 
-    public void setActivationKey(String activationKey) {
-        this.activationKey = activationKey;
-    }
+	public String getActivationKey() {
+		return activationKey;
+	}
 
-    public String getLangKey() {
-        return langKey;
-    }
+	public void setActivationKey(String activationKey) {
+		this.activationKey = activationKey;
+	}
 
-    public void setLangKey(String langKey) {
-        this.langKey = langKey;
-    }
+	public String getLangKey() {
+		return langKey;
+	}
 
-    public Set<Authority> getAuthorities() {
-        return authorities;
-    }
+	public void setLangKey(String langKey) {
+		this.langKey = langKey;
+	}
 
-    public void setAuthorities(Set<Authority> authorities) {
-        this.authorities = authorities;
-    }
+	public String getImageUrl() {
+		return imageUrl;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 
-        User user = (User) o;
+	public Set<Authority> getAuthorities() {
+		return authorities;
+	}
 
-        if (!login.equals(user.login)) {
-            return false;
-        }
+	public void setAuthorities(Set<Authority> authorities) {
+		this.authorities = authorities;
+	}
 
-        return true;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
-    @Override
-    public int hashCode() {
-        return login.hashCode();
-    }
+		User user = (User) o;
 
-    @Override
-    public String toString() {
-        return "User{" + "login='" + login + '\'' + ", password='" + password + '\'' + ", firstName='" + firstName
-                + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", activated='" + activated
-                + '\'' + ", isTeacher='" + isTeacher + '\'' + ", langKey='" + langKey + '\'' + ", activationKey='"
-                + activationKey + '\'' + "}";
-    }
+		if (!login.equals(user.login)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return login.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "User{" + "login='" + login + '\'' + ", password='" + password
+				+ '\'' + ", firstName='" + firstName + '\'' + ", lastName='"
+				+ lastName + '\'' + ", email='" + email + '\''
+				+ ", activated='" + activated + '\'' + ", isTeacher='"
+				+ isTeacher + '\'' + ", langKey='" + langKey + '\''
+				+ ", imageUrl='" + imageUrl + '\'' + ", activationKey='"
+				+ activationKey + '\'' + "}";
+	}
 }
