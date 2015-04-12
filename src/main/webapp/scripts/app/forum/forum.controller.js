35d367d1ef07f77faf34ad34de42f10439ca6178
conchigp <Usuario@10.0.2.15>
2015-04-12 21:21:42 +0200

    Inicio Foro

---
+++ src/main/webapp/scripts/app/forum/forum.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('teamstudyApp')
+    .controller('ForumController', function ($stateParams,$scope,Thread, Message, Reply) {
+    	
+    	
+ 
+           
+        });
