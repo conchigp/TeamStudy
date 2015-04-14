9bae282416aab2d9226200bdd94be71a227c9390
diegogalocha <diegogalocha@dg-virtual-machine>
2015-04-14 14:40:18 +0200

    Lista de alumnos según el grupo que se pulse en el sidebarright.

--- src/main/webapp/scripts/app/main/sidebar-right.controller.js
+++ src/main/webapp/scripts/app/main/sidebar-right.controller.js
@@ -1,8 +1,29 @@
 'use strict';
 
 angular.module('teamstudyApp').controller('SidebarrightController',
-		function($scope, GroupList, Principal) {
-			$scope.isInRole = Principal.isInRole;
-			
+		function($stateParams,$state, $scope,StudentsCRUD, StudentsListAll,Principal) {
+			Principal.identity().then(function(account) {
+				$scope.account = account;
+				$scope.isAuthenticated = Principal.isAuthenticated;
+				$scope.isInRole = Principal.isInRole;
+				
+				}).then(function() {
+					
+					var groupId = $stateParams.groupId;
+					$scope.groupId = groupId;
+					
+					StudentsCRUD.get({
+						groupId : groupId
+					}, function(result) {
+						$scope.students = result.data;
+
+					});
+								
+					StudentsListAll.get(function(result) {
+						$scope.studentsAll = result.data;
+						
+					});
+					
+				});
 
 		});
---
+++ src/main/webapp/scripts/components/sidebar/sidebar-right.service.js
@@ -0,0 +1,43 @@
+(function() {
+	'use strict';
+
+	function StudentsListAll($resource) {
+		return $resource('api/students', {}, {
+			'get' : {
+				method : 'GET',
+				params : {},
+				isArray : true,
+				interceptor : {
+					response : function(response) {
+						// expose response
+						return response;
+					}
+				}
+
+			}
+
+		});
+	}
+	function StudentsCRUD($resource) {
+		return $resource('api/student', {}, {
+			'get' : {
+				method : 'GET',
+				params : {
+					groupId : '@groupId'
+				},
+				isArray : true,
+				interceptor : {
+					response : function(response) {
+						// expose response
+						return response;
+					}
+				}
+
+			}
+		});
+	}
+
+	angular.module('teamstudyApp').factory('StudentsListAll', StudentsListAll)
+	.factory('StudentsCRUD', StudentsCRUD);
+
+})();
