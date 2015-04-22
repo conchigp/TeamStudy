f4de8ae8b77d19bfc53da6ee94ec27de217fa4eb
juanmartin1992 <juanmartin1892@gmail.com>
2015-04-22 10:25:22 +0200

    menu bar content bug fix

--- src/main/webapp/scripts/app/wiki/wiki.controller.js
+++ src/main/webapp/scripts/app/wiki/wiki.controller.js
@@ -1,14 +1,19 @@
 'use strict';
 
 angular.module('teamstudyApp').controller('WikiController',
-		function($stateParams, $scope, GroupCRUDAdmin) {
-
+		function($stateParams, $scope, GroupCRUDAdmin,Principal) {
+			
 			var groupId = localStorage.getItem('groupId');
-
 			$scope.groupId = groupId;
-
 			$scope.group = GroupCRUDAdmin.get({
 				groupId : groupId
 			});
-
+			
+			Principal.identity().then(function(account) {
+				$scope.account = account;
+				$scope.isAuthenticated = Principal.isAuthenticated;
+				$scope.isInRole = account.roles;
+			}).then(function() {
+				
+			});
 		});
