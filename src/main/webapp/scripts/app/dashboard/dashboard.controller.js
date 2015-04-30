8c404aa6688c48246f65e45bd59fa5f2ea1d0dea
diegogalocha <diegogalocha@dg-virtual-machine>
2015-04-30 14:02:52 +0200

    Sigo intentando las super gráficas de Morris... :S

--- src/main/webapp/scripts/app/dashboard/dashboard.controller.js
+++ src/main/webapp/scripts/app/dashboard/dashboard.controller.js
@@ -28,7 +28,8 @@ angular.module('teamstudyApp')
 			    			groupId : groupId
 						},function(result) {
 			    			$scope.threadsAll = result.data;
-			    			
+			    			var array = [];
+			    						    			
 			    			$scope.threadsAll.forEach(function (item) {
 			    				
 			    				MessageList.get({
@@ -36,19 +37,39 @@ angular.module('teamstudyApp')
 			    				},function(result){
 			    					var mensajes = result.data;
 			    					item.tamano = mensajes;
+			    					array.push(item.title, item.tamano.length);
 			    				});
-			    				
 			    			});
+			    			$scope.array=array;
 			    		});
-			    		function getArray(){
-							var array = [];
-							$scope.threadsAll.forEach(function(item){
-								array.push(item.title, item.tamano);
-							});
-							return $scope.array=array;
-						}
-						getArray();
+			    	}).then(function(){
+			    		
+			    		console.log($scope.array);
+			    		new Morris.Line({
+	    				// ID of the element in which to draw the chart.
+	    				element : 'myfirstchart',
+	    				// Chart data records -- each entry in this array corresponds to a point on
+	    				// the chart.
+	    				data : [
+	    				        { year: '2008', value: 20 },
+	    				        { year: '2009', value: 10 },
+	    				        { year: '2010', value: 5 },
+	    				        { year: '2011', value: 5 },
+	    				        { year: '2012', value: 20 }
+	    				      ],
+	    				// The name of the data record attribute that contains x-values.
+	    				xkey : 'year',
+	    				// A list of names of data record attributes that contain y-values.
+	    				ykeys : [ 'value' ],
+	    				// Labels for the ykeys -- will be displayed when you hover over the
+	    				// chart.
+	    				labels : [ 'Value' ]
+	    			});
+			    	
 			    	});
+					
+					
+					
 				});
 
 
