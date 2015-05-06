dd19404c3aa6831bdf3b15d4758728c227d2961d
diegogalocha <diegogalocha@dg-virtual-machine>
2015-05-06 16:26:38 +0200

    Pequeños retoques y pruebas.

--- src/main/webapp/scripts/app/main/sidebar-right.controller.js
+++ src/main/webapp/scripts/app/main/sidebar-right.controller.js
@@ -10,10 +10,16 @@ angular.module('teamstudyApp').controller(
 				$scope.isInRole = Principal.isInRole;
 
 			}).then(function() {
-
+				
+//				function scrollDiv(){
+//				    var div = document.getElementById('scrolldiv');
+//				    console.log('Estoy dentro');
+//				    div.scrollTop = '9999';
+//				}
+				
 				var groupId = localStorage.getItem('groupId');
 				$scope.groupId = groupId;
-
+				
 				StudentsCRUD.get({
 					groupId : groupId
 				}, function(result) {
