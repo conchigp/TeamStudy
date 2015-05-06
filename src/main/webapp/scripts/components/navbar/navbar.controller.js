dd155c139dcecb153cf5e6a6e54a761910feb0c1
conchigp <Usuario@10.0.2.15>
2015-05-06 15:42:10 +0200

    navbar

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -2,6 +2,11 @@
 
 angular.module('teamstudyApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
+    	
+    	 Principal.identity().then(function(account) {
+             $scope.account = account;
+         });
+    	 
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.isInRole = Principal.isInRole;
         $scope.$state = $state;
